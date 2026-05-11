6500576cc05952cab10e96de63c7c4da2cec06ae
bevan <bevansalter@proton.me>
2026-05-11 21:42:05 +0100

    Fixed colour picker isseu

--- QrCodeGenerator/QrCodeGenerator.Ui/ViewModels/MainWindowViewModel.cs
+++ QrCodeGenerator/QrCodeGenerator.Ui/ViewModels/MainWindowViewModel.cs
@@ -4,27 +4,7 @@ namespace QrCodeGenerator.Ui.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
-    
-    private Color _lightColour = Colors.Black;
+    public Color LightColour { get; set; } = Colors.White;
 
-    public Color LightColour
-    {
-        get => _lightColour;
-        set
-        {
-            _lightColour = value;
-        }
-    }
-    
-    private Color _darkColour = Colors.Black;
-
-    public Color DarkColour
-    {
-        get => _darkColour;
-        set
-        {
-            _darkColour = value;
-        }
-    }
+    public Color DarkColour { get; set; } = Colors.White;
 }
